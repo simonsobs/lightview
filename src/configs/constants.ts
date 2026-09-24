3c60a5713d0d715f1b2c2180f6d5fd0447e141d8
Jeremy Myers <inernestmusic@gmail.com>
2026-09-24 12:48:49 -0400

    Fix broken crossmatcher link out and tweak link-out class

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -38,6 +38,8 @@ export const DEFAULT_HOMEPAGE_PLOT_LAYOUT = {
 export const MIN_MAX_FLUX_VALUES = [0.001, 5];
 
 export const SIMBAD_BASE_URL = 'https://simbad.cds.unistra.fr';
+export const SIMBAD_BASE_LINK_OUT_URL =
+  'https://portal.cds.unistra.fr/?target=';
 
 /** Search/beam radius used for unassigned-source cross-matching (SIMBAD cone search, nearby
  * unassigned sources, and the sky plot's dashed beam circle). */
